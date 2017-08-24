adf5c56d22fa6cf079526607db170447ecba4e27
Robert Aguirre <RobertAguirre@users.noreply.github.com>
2017-08-24 16:28:36 -0400

    model naming

---
+++ app/models/admin_user.rb
@@ -0,0 +1,9 @@
+class AdminUser < ApplicationRecord
+
+  #Since we renamed table admin_users
+  #Also when working with legacy databases that dont follow convention
+    # self.table_name = "admin_users"
+
+    # -- Or -- change the class name and file name
+
+end
