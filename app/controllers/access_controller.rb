c8b774fff2667d609cf91c91fe114d2625f68acf
Robert Aguirre <RobertAguirre@users.noreply.github.com>
2017-09-04 18:30:56 -0400

    login and logout

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -11,9 +11,29 @@ class AccessController < ApplicationController
   end
 
   def attempt_login
+    if params[:username].present? && params[:password].present?
+      found_user = AdminUser.where(:username => params[:username]).first
+      if found_user
+        authorized_user = found_user.authenticate(params[:password])
+      end
+    end
+
+    if authorized_user
+      session[:user_id] = authorized_user.id
+      flash[:notice] = "You are now logged in."
+      redirect_to(admin_path)
+    else
+      # flash.now for current request vs next as with redirect_to
+      flash.now[:notice] = "Invalid username/password combination."
+      render('login')
+    end
+
   end
 
   def logout
+    session[:user_id] = nil
+    flash[:notice] = 'Logged out'
+    redirect_to(access_login_path)
   end
 
 end
