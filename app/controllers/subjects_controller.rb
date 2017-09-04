576b71615a37746d2db75db5ca063d3f78ce34f9
Robert Aguirre <RobertAguirre@users.noreply.github.com>
2017-09-04 15:05:30 -0400

    controller filters

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -2,6 +2,8 @@ class SubjectsController < ApplicationController
 
   layout 'admin'
 
+  before_action :set_subject_count, :only => [:new, :create, :edit, :update]
+
   def index
     @subjects = Subject.sorted
   end
@@ -12,7 +14,6 @@ class SubjectsController < ApplicationController
 
   def new
     @subject = Subject.new({:name => 'Default'})
-    @subject_count = Subject.count + 1
   end
 
   def create
@@ -25,14 +26,12 @@ class SubjectsController < ApplicationController
       redirect_to(subjects_path)
     else
       #If save fails, redisplay the form so the user can fix the problem
-      @subject_count = Subject.count + 1
       render('new')
     end
   end
 
   def edit
     @subject = Subject.find(params[:id])
-    @subject_count = Subject.count
   end
 
   def update
@@ -45,7 +44,6 @@ class SubjectsController < ApplicationController
       redirect_to(subject_path(@subject))
     else
       #If save fails, redisplay the form so the user can fix the problem
-      @subject_count = Subject.count
       render('edit')
     end
   end
@@ -65,6 +63,13 @@ class SubjectsController < ApplicationController
 
   def subject_params
     params.require(:subject).permit(:name, :position, :visible, :created_at)
+  end
+
+  def set_subject_count
+    @subject_count = Subject.count
+    if params[:action] == 'new' || params[:action] == 'create'
+      @subject_count += 1
+    end
   end
 
 
