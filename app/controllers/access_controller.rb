7b93ef5ff02d7330e71fdc62a201f8bff6ec2462
Robert Aguirre <RobertAguirre@users.noreply.github.com>
2017-09-06 19:54:16 -0400

    finishing touches

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -6,7 +6,7 @@ class AccessController < ApplicationController
   before_action :confirmed_logged_in, :except => [:login, :attempt_login, :logout]
 
   def menu
-    # display text and links
+    @username = session[:username]
   end
 
   def login
@@ -23,6 +23,7 @@ class AccessController < ApplicationController
 
     if authorized_user
       session[:user_id] = authorized_user.id
+      session[:username] = authorized_user.username
       flash[:notice] = "You are now logged in."
       redirect_to(admin_path)
     else
@@ -35,6 +36,7 @@ class AccessController < ApplicationController
 
   def logout
     session[:user_id] = nil
+    session[:username] = nil
     flash[:notice] = 'Logged out'
     redirect_to(access_login_path)
   end
